b45691f73d351d4bd2d64eb15e49b9a5202287ae
marin <marinkitagawa.x86@gmail.com>
2023-06-21 06:59:18 -0400

    small fixes

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,10 +1,9 @@
-
 import Link from 'next/link';
 
 export default function Button(props: any) {
   return (
     <div>
-      <div className="w-fit border-2 border-white hover:scale-110  transition">
+      <div className="w-fit border-2 border-white transition  hover:scale-110">
         <div className="px-8 pb-[0.60rem] pt-2">
           {' '}
           <Link href={props.href} className="">
--- src/app/components/fancyButton.tsx
+++ src/app/components/fancyButton.tsx
@@ -2,17 +2,20 @@ import Link from 'next/link';
 
 export default function FancyButton(props: any) {
   return (
-    <Link href={props.href} className=" hover:underline hidden md:block">
-    <button type="button" class="md:inline-block rounded-[12px] bg-gradient-to-b from-[#04D2A9] to-[#0AA48A] leading-none p-px shadow-sm hover:scale-110 transition">
-<span class="md:inline-block rounded-[11px] bg-gradient-to-b from-[#4DDFC2] to-[#04D2A9] p-px hover:slac">
-<span class="md:inline-flex rounded-[10px] items-center gap-x-1.5 bg-[#04D2A9] px-2 py-1 text-sm text-bold text-white">
-
-
-{props.text}
-<span class="md:inline-flex h-5 w-5 items-center justify-center rounded-md bg-[#03BE99] text-xs ml-0.5">{props.svg}</span>
-</span>
-</span>
-</button>
+    <Link href={props.href} className=" hidden hover:underline md:block">
+      <button
+        type="button"
+        className="rounded-[12px] bg-gradient-to-b from-[#04D2A9] to-[#0AA48A] p-px leading-none shadow-sm transition hover:scale-110 md:inline-block"
+      >
+        <span className="hover:slac rounded-[11px] bg-gradient-to-b from-[#4DDFC2] to-[#04D2A9] p-px md:inline-block">
+          <span className="text-bold items-center gap-x-1.5 rounded-[10px] bg-[#04D2A9] px-2 py-1 text-sm text-white md:inline-flex">
+            {props.text}
+            <span className="ml-0.5 h-5 w-5 items-center justify-center rounded-md bg-[#03BE99] text-xs md:inline-flex">
+              {props.svg}
+            </span>
+          </span>
+        </span>
+      </button>
     </Link>
   );
 }
